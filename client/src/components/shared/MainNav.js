b9f800e41d4ec78ed4c62fdfc83f8a82386bc73a
steph medlin <stephanieanndavis13@gmail.com>
2022-05-12 11:30:37 -0600

    updated logo

--- client/src/components/shared/MainNav.js
+++ client/src/components/shared/MainNav.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import { Navbar, Offcanvas, Container, Nav } from 'react-bootstrap';
-// import { Navbar } from '../../styles/sharedStyles.js';
+import logo from '../..'
 
 const MainNav = ({}) => {
   return (
     <>
           <Navbar style={{backgroundColor: "#2D0363"}} expand={false}>
             <Container style={{padding:"0px !important"}}>
-              <Navbar.Brand href="/"><img src="/logo.png" style={{height: "150px", width: "150px"}}/></Navbar.Brand>
+              <Navbar.Brand href="/"><img src="https://i0.wp.com/bikepackingwander.files.wordpress.com/2022/05/logo.png?ssl=1" style={{height: "150px", width: "150px"}}/></Navbar.Brand>
               <Navbar.Toggle aria-controls="offcanvasNavbar" />
               <Navbar.Offcanvas
                 id="offcanvasNavbar"
